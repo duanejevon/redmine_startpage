a9223742956f86599cc48ffc13d3ae5ff9d62769
Duane Hughes <duanejevon@gmail.com>
2025-05-26 17:00:12 +0200

    Bump version

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_startpage do
   name 'Redmine Startpage plugin'
   author 'Txinto Vaz'
   description 'This is a plugin for Redmine.  It allows the user to select almost any Redmine sub page as the start page for a Redmine website'
-  version '0.2.0'
+  version '0.2.2'
   url 'https://github.com/txinto/redmine_startpage'
   
   settings :default => {
